8431275846d24eac7902dfab870b53ffb3640eef
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-12-01 19:17:02 +0900

    Feat: Implement create consulting message

--- src/pages/Consulting.tsx
+++ src/pages/Consulting.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { postAPI } from "../axios";
 import ConsultingLeftSideBar from "../components/layout/ConsultingLeftSideBar";
 import DefaultLayout from "../components/layout/DefaultLayout";
 import Wrapper from "../components/layout/Wrapper";
@@ -7,7 +8,9 @@ import useModal from "../hooks/useModal";
 
 function Consulting() {
   const [selectedUser, setSelectedUser] = useState<any>(null);
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState<string>("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
 
   const {
     isOpen: isUserSearchModalOpen,
@@ -17,6 +20,45 @@ function Consulting() {
   } = useModal();
 
   const currentUser = JSON.parse(localStorage.getItem("user") || "{}");
+
+  const submitConsultingMessage = async () => {
+    try {
+      const formattedStartDate = getISODateTime(startDate);
+      const formattedEndDate = getISODateTime(endDate);
+
+      await postAPI("/consulting", {
+        content: message,
+        studentId: selectedUser.userId,
+        consultantId: currentUser.userId,
+        startDate: formattedStartDate,
+        endDate: formattedEndDate,
+      });
+      setSelectedUser(null);
+      setMessage("");
+      setStartDate("");
+      setEndDate("");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleStartDateChange = (date: any) => {
+    setStartDate(date);
+
+    const startDateObj = new Date(date);
+    const endDateObj = new Date(startDateObj);
+    endDateObj.setDate(startDateObj.getDate() + 6);
+
+    const formattedEndDate = `${endDateObj.getFullYear()}-${
+      endDateObj.getMonth() + 1
+    }-${endDateObj.getDate()}`;
+    setEndDate(formattedEndDate);
+  };
+
+  const getISODateTime = (dateString: any) => {
+    const date = new Date(dateString);
+    return date.toISOString();
+  };
 
   return (
     <DefaultLayout>
@@ -31,6 +73,20 @@ function Consulting() {
                   받는사람: 고{selectedUser.grade} {selectedUser.className}{" "}
                   {selectedUser.userName}
                 </div>
+                <div>
+                  컨설팅 주차:{" "}
+                  <input
+                    type="date"
+                    value={startDate}
+                    onChange={(e) => handleStartDateChange(e.target.value)}
+                  />{" "}
+                  ~{" "}
+                  <input
+                    type="date"
+                    value={endDate}
+                    onChange={(e) => handleStartDateChange(e.target.value)}
+                  />
+                </div>
                 <textarea
                   className="w-full h-40 mt-4 p-2 border rounded"
                   placeholder="컨설팅 내용을 입력해주세요."
@@ -39,7 +95,7 @@ function Consulting() {
                 />
                 <button
                   className="w-24 h-10 mt-4 self-end bg-blue-500 text-white rounded p-2 hover:bg-blue-700"
-                  onClick={() => {}}
+                  onClick={submitConsultingMessage}
                 >
                   보내기
                 </button>
