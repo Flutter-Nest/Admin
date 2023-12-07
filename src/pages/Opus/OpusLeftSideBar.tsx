793b3077b72e5f779b9d3c26dfe996ac02d4c346
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-12-07 12:34:32 +0900

    Chore: Remove unused codes

--- src/pages/Opus/OpusLeftSideBar.tsx
+++ src/pages/Opus/OpusLeftSideBar.tsx
@@ -17,28 +17,18 @@ function OpusLeftSideBar() {
             <div className="text-white">{currentUser.userName}</div>
           </div>
         </div>
-        <button className="w-full h-10 flex justify-between p-4 text-white hover:bg-[#2C3238]">
-          <div className="flex h-full gap-4 items-center"></div>
+        <button
+          onClick={() => handleMode("opus")}
+          className="w-full h-10 flex justify-between p-4 text-white hover:bg-[#2C3238]"
+        >
+          <div className="flex h-full gap-4 items-center">OPUS 등록</div>
         </button>
-        <div className="w-full h-10 flex justify-between p-4 text-white cursor-default">
-          <div className="flex h-full gap-4 items-center">
-            <span></span>
-          </div>
-          <div className="flex h-full items-center"></div>
-        </div>
-        {/* <div>
-          {boards?.map((board: BoardType) => (
-            <button
-              key={board.boardId}
-              className="w-full h-10 flex justify-between p-4 text-white hover:bg-[#2C3238] items-center"
-              onClick={() =>
-                navigate(`/workspaces/${workspaceId}/boards/${board.boardId}`)
-              }
-            >
-              {board.boardName}
-            </button>
-          ))}
-        </div> */}
+        <button
+          onClick={openUserSearchModal}
+          className="w-full h-10 flex justify-between p-4 text-white hover:bg-[#2C3238]"
+        >
+          <div className="flex h-full gap-4 items-center">컨설팅 등록</div>
+        </button>
       </div>
     </>
   );
