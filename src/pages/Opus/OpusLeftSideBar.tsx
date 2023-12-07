9003c588947e5938361a0ca396fa6366680620d1
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-12-07 17:27:07 +0900

    Feat: Implement create opus and consulting on opus tab

--- src/pages/Opus/OpusLeftSideBar.tsx
+++ src/pages/Opus/OpusLeftSideBar.tsx
@@ -1,5 +1,15 @@
-function OpusLeftSideBar() {
+import { MdOutlineMessage } from "react-icons/md";
+import { PiTelevisionSimpleBold } from "react-icons/pi";
+
+function OpusLeftSideBar({
+  openUserSearchModal,
+  handleMode,
+}: {
+  openUserSearchModal: () => void;
+  handleMode: (mode: string) => void;
+}) {
   const currentUser = JSON.parse(localStorage.getItem("user") || "{}");
+
   return (
     <>
       <div className="h-full min-w-[320px] bg-[#161A1E]">
@@ -21,13 +31,19 @@ function OpusLeftSideBar() {
           onClick={() => handleMode("opus")}
           className="w-full h-10 flex justify-between p-4 text-white hover:bg-[#2C3238]"
         >
-          <div className="flex h-full gap-4 items-center">OPUS 등록</div>
+          <div className="flex h-full gap-4 items-center">
+            <PiTelevisionSimpleBold />
+            OPUS 등록
+          </div>
         </button>
         <button
           onClick={openUserSearchModal}
           className="w-full h-10 flex justify-between p-4 text-white hover:bg-[#2C3238]"
         >
-          <div className="flex h-full gap-4 items-center">컨설팅 등록</div>
+          <div className="flex h-full gap-4 items-center">
+            <MdOutlineMessage />
+            컨설팅 등록
+          </div>
         </button>
       </div>
     </>
